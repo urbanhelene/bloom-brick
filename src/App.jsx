6a1208a6dbc300a0f9d9787711e0c5ed96349c5d
heleneurban <cc231043@fhstp.ac.at>
2024-04-21 17:27:37 +0200

    alles was wir am Sonntag gemacht haben

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Movies} from "./Movies.jsx";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Cards from './components/Cards';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        <h1>Movies!</h1>
-        <Movies />
-    </>
-  )
+    return (
+        <div>
+            <Navbar />
+            <Cards />
+        </div>
+    );
 }
 
-export default App
+export default App;
